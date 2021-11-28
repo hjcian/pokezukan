335b1ca13de0e47997af14249b0e33e41ffcd013
Max Cian <maxcian@jubo.health>
2021-11-28 21:12:20 +0800

    docs(poke-list.json): add data

--- scripts/extract-pokemon.py
+++ scripts/extract-pokemon.py
@@ -6,7 +6,12 @@ dirname = Path(__file__).resolve().parent
 
 
 def is_pokemon_row(cells):
-    return len(cells) > 0 and "#" in cells[0].get_text()
+    return len(cells) > 0 and (
+        "#" in cells[0].get_text() or
+        "#" in cells[1].get_text() or
+        "#" in cells[2].get_text() or
+        "#" in cells[3].get_text()
+    )
 
 
 def extract_types_from_typecells(cell_1, cell_2):
@@ -23,11 +28,12 @@ def extract_types_from_typecells(cell_1, cell_2):
 
 def find_start(cells):
     # 只對全國圖鑑編號感興趣
+    # 找到 index 最大且有 "#" 在文字中的，就是全國圖鑑編號
     start = 0
     for i, cell in enumerate(cells):
         if '#' in cell.get_text():
             start = i
-        else:
+        if i > 4:
             break
     return start
 
@@ -59,14 +65,22 @@ if __name__ == "__main__":
     tables = soup.find_all('table')
     tables = [table for table in tables if 'eplist' in table.attrs['class']]
 
+    total_pokemons = []
     for i, table in enumerate(tables):
-        # table = tables[1]
-        if i != 5:
-            continue
+
+        # if i != 8:
+        #     continue
+
         print("table:", i)
         rows = table.find('tbody').find_all('tr')
         rows = [r for r in rows if is_pokemon_row(r.find_all('td'))]
 
         # print(json.dumps(get_meta(rows[0]), indent=2, ensure_ascii=False))
-        metas = [get_meta(r) for r in rows]
-        print(json.dumps(metas, indent=2, ensure_ascii=False))
+        pokemons = [get_meta(r) for r in rows]
+        # print(json.dumps(pokemons, indent=2, ensure_ascii=False))
+
+        total_pokemons.extend(pokemons)
+    print(len(total_pokemons))
+
+    json.dump(total_pokemons, open(dirname/'poke-list.json', 'w'),
+              indent=2, ensure_ascii=False)
