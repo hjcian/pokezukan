9d6a41c99e64fd000ee1e505f3e964cd5ebf573c
H.-J. Max Cian <chutarojian@gmail.com>
2022-11-18 15:50:25 +0800

    Add name search (#28)
    * chore(wip): add dataset and validate it
    
    * docs(readme): update
    
    * chore(zukan): add search engine
    
    * feat(poke search): finish implementation
    
    * build(node version): specify node version
    
    Co-authored-by: Max Cian <maxcian@jubo.health>

--- src/Components/AttrSelector.js
+++ src/Components/AttrSelector.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { Dropdown, Responsive} from 'semantic-ui-react'
+import React from "react";
+import { Dropdown, Responsive } from "semantic-ui-react";
 
-import { attributes, attrIconNames, attrBaseColors } from '../constants/const'
-import './AttrSelector.css'
+import { attributes, attrIconNames, attrBaseColors } from "../constants/const";
+import "./AttrSelector.css";
 
 const options = attributes.map((item, idx) => {
-  return { 
-    key: idx, 
-    value: idx, 
+  return {
+    key: idx,
+    value: idx,
     text: item,
-    label: { 
-      color: attrBaseColors[idx], 
-      icon: attrIconNames[idx]
-    }
-  }})
+    label: {
+      color: attrBaseColors[idx],
+      icon: attrIconNames[idx],
+    },
+  };
+});
 
-const AttrSelectorContent = ({options, renderLabel, handleChange}) => {
+const AttrSelectorContent = ({ options, renderLabel, handleChange }) => {
   return (
     <Dropdown
       multiple
@@ -23,48 +24,51 @@ const AttrSelectorContent = ({options, renderLabel, handleChange}) => {
       fluid
       search
       options={options}
-      placeholder='選擇防守方屬性'
+      placeholder="選擇防守方屬性"
       renderLabel={renderLabel}
       onChange={handleChange}
     />
-  )
-}
+  );
+};
 
-const MobileBoundary = 768
+const MobileBoundary = 768;
 export default class AttrSelector extends React.Component {
   state = {
     selectedOptions: null,
-  }
+  };
   handleChange = (e, data) => {
-    e.preventDefault()
+    e.preventDefault();
     if (data.value.length > 2) {
-      data.value.shift()
+      data.value.shift();
     }
-    this.setState({ selectedOptions: data.value }, ()=>{
-      this.props.setAttrs(this.state.selectedOptions)
-      console.log(`Option selected:`, JSON.stringify(this.state.selectedOptions, null, 4))      
-    })
-  }
+    this.setState({ selectedOptions: data.value }, () => {
+      this.props.setAttrs(this.state.selectedOptions);
+      console.log(
+        `Option selected:`,
+        JSON.stringify(this.state.selectedOptions, null, 4)
+      );
+    });
+  };
   renderLabel = (label) => ({
     color: attrBaseColors[label.key],
     content: label.text,
-    icon: attrIconNames[label.key] || 'question circle',
-  })
-  render () {
+    icon: attrIconNames[label.key] || "question circle",
+  });
+  render() {
     return (
       <div className="attr-selector-container">
-        <Responsive maxWidth={MobileBoundary-1}>
+        <Responsive maxWidth={MobileBoundary - 1}>
           <div className="attr-selector-mobile">
-            <AttrSelectorContent  
+            <AttrSelectorContent
               options={options}
               renderLabel={this.renderLabel}
               handleChange={this.handleChange}
-              />
+            />
           </div>
         </Responsive>
         <Responsive minWidth={MobileBoundary}>
           <div className="attr-selector">
-            <AttrSelectorContent  
+            <AttrSelectorContent
               options={options}
               renderLabel={this.renderLabel}
               handleChange={this.handleChange}
@@ -72,6 +76,6 @@ export default class AttrSelector extends React.Component {
           </div>
         </Responsive>
       </div>
-    )
+    );
   }
 }
---
+++ src/zukan/search.js
@@ -0,0 +1,57 @@
+import rawZukan from "./raw_zukan";
+import { attributes, attrIconNames, attrBaseColors } from "../constants/const";
+
+import * as CSV from "csv-string";
+import flexsearch from "flexsearch";
+
+const { Index } = flexsearch;
+
+const twIndex = new Index({
+  // eslint-disable-next-line no-control-regex
+  encode: (str) => str.replace(/[\x00-\x7F]/g, "").split(""),
+});
+const zukanIdx = {};
+
+const findAttrIconAndColor = (attr) => {
+  if (!attr) return [-1, null, null];
+
+  const attrIdx = attributes.indexOf(attr);
+  if (attrIdx === -1) return [attrIdx, null, null];
+  return [attrIdx, attrIconNames[attrIdx], attrBaseColors[attrIdx]];
+};
+
+(function init() {
+  const rows = CSV.parse(rawZukan);
+  rows.forEach((row, idx) => {
+    if (idx === 0) {
+      return;
+    }
+    const [attridx1, icon1, color1] = findAttrIconAndColor(row[4]);
+    const [attridx2, icon2, color2] = findAttrIconAndColor(row[5]);
+    const pokemon = {
+      idx: row[0],
+      title: row[1],
+      tw: row[1],
+      jp: row[2],
+      en: row[3],
+      type1: row[4],
+      type2: row[5],
+      attridx1,
+      attridx2,
+      icon1,
+      icon2,
+      color1,
+      color2,
+    };
+    zukanIdx[row[0]] = pokemon;
+    twIndex.add(row[0], row[1]);
+  });
+})();
+
+export const search = (word) => {
+  const results = [];
+  twIndex.search(word).forEach((idx) => {
+    results.push(zukanIdx[idx]);
+  });
+  return results;
+};
